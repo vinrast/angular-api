ef3e1bc03e402ca848ce2387eb082f2330a65fcd
vincen <vinrast@gmail.com>
2019-12-05 18:25:58 -0300

    primer commit

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import { App } from './application';
+import { ApplicationConfig } from '@loopback/core';
+
+export { App };
+
+export async function main(options: ApplicationConfig = {}) {
+  const app = new App(options);
+  await app.boot();
+  await app.migrateSchema();
+  await app.start();
+
+  const url = app.restServer.url;
+  console.log(`Server is running at ${url}`);
+  console.log(`Try ${url}/ping`);
+
+  return app;
+}
---
+++ src/models/user.model.ts
@@ -0,0 +1,52 @@
+import { Entity, model, property } from '@loopback/repository';
+
+@model()
+export class User extends Entity {
+  @property({
+    type: 'number',
+    id: true,
+    generated: true,
+  })
+  id?: number;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  name: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  cpf: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  email: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  phone: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  address: string;
+
+
+  constructor(data?: Partial<User>) {
+    super(data);
+  }
+}
+
+export interface UserRelations {
+  // describe navigational properties here
+}
+
+export type UserWithRelations = User & UserRelations;
